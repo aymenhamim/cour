b72a173f667eed50766756b1c07534ba8ad4ca50
Aymen Hamim <aymenhamim2004@gmail.com>
2024-12-26 10:47:40 +0100

    TP12-1

---
+++ TP12/tp12-1/src/App.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import Form from "./components/Form";
+import "../src/index.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
